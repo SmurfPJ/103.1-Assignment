bfcfb4a53b480720dec8c7a73518dcb21f1226b6
SmurfPJ <103091093+SmurfPJ@users.noreply.github.com>
2022-05-25 15:58:59 +1200

    Update Task 4.cpp

--- Task 4/Task 4/Task 4.cpp
+++ Task 4/Task 4/Task 4.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+#include <string>
 
-int hangMan()
+int Hangman()
 {
     std::cout << "   _____ \n";
     std::cout << "  |     | \n";
@@ -15,17 +16,49 @@ int hangMan()
     return;
 }
 
+int HangmanRight()
+{
+    std::cout << "\n\n\tYour guess is correct\n";
+    std::cout << " O \n";
+    std::cout << "\\|/ \n";
+    std::cout << " | \n";
+    std::cout << "/ \\ ";
+    std::cout << "\n\n";
+}
+
+int HangmanFailed()
+{
+    std::cout << "\n\n\tYour Guess is wrong\n";
+    std::cout << "   _____ \n";
+    std::cout << "  |     | \n";
+    std::cout << "  |    /|\\ \n";
+    std::cout << "  |     | \n";
+    std::cout << "  |    /|\\ \n";
+    std::cout << "  |     O \n";
+    std::cout << "  | \n";
+    std::cout << "  | \n";
+    std::cout << "-----";
+    std::cout << "\n\n";
+}
+
+int LetterCheck(char check, int round)
+{
+    if (check == 'e')
+        HangmanRight();
+    else if (round < 2)
+        std::cout << "\nSorry, try again\n";
+    return;
+}
+
 int main()
 {
-    hangMan();
-    int word = rand() % 3;
-
-    switch (word) {
-    case 0:
-        break;
-    case 1:
-        break;
-    case 2:
-        break;
+    Hangman();
+    char letter;
+    
+    for (int i = 0; i < 3; i++) {
+        std::cout << "Guess the missing letter in Yo_bee: ";
+        std::cin >> letter;
+        LetterCheck(letter, i);
     }
+    HangmanFailed();
 }
